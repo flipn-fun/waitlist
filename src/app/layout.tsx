50afd8597c1f65aae83a0da21534872fa29819ca
Emily <emily@ref.finance>
2025-02-05 14:47:44 +0800

    add ga.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -18,6 +19,23 @@ export default function RootLayout({
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0,user-scalable=no"
+        />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z1C57YHPTN"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Z1C57YHPTN');
+            `
+          }}
         />
       </head>
       <body className={`antialiased`}>{children}</body>
