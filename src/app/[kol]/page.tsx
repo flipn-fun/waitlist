8dbb18654b76ce6a477a39e8883fb009e8b002b0
Emily <emily@ref.finance>
2025-02-11 10:04:40 +0800

    add kol report.

--- src/app/[kol]/page.tsx
+++ src/app/[kol]/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
 import Home from "@/sections/home";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+import kols from "../../config/kol";
 
 export default function KolPage() {
+  const params = useParams();
+
+  useEffect(() => {
+    const kol = params.kol as string;
+    if (!kol) return;
+    if (!kols[kol]) return;
+    fetch("https://api.stg.dumpdump.fun/api/v1/visit", {
+      method: "POST",
+      body: JSON.stringify({
+        kol_code: kols[kol]
+      })
+    });
+  }, [params]);
   return <Home />;
 }
